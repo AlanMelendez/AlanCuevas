6e066beb9495d9a2ef64ce9f70de11d8e0f22d80
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2022-08-20 01:07:33 -0500

    navbar fallando

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,10 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+
 
 
 
@@ -16,6 +20,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
