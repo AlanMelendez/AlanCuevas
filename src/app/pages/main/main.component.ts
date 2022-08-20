54b0519484f42514a6613757012b527c9661532d
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2022-08-20 14:24:34 -0500

    pagina lista

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild ,TemplateRef} from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  
+  @ViewChild('abilites-contain') abilites!: TemplateRef<any>;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.abilites.elementRef.nativeElement)
   }
 
 }
